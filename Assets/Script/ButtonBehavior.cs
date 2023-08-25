28d027871718178d4c65da56b0e22b4e988b8d47
Wafflgeasm <nathanael.green@g.austincc.edu>
2023-08-24 21:42:30 -0500

    Electricity User Update

--- Assets/Script/ButtonBehavior.cs
+++ Assets/Script/ButtonBehavior.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class ButtonBehavior : MonoBehaviour, IInteractable
 {
-    [SerializeField] private MeshRenderer immisives;
-    [SerializeField][ColorUsage(false,true)] private Color onEmission, offEmission;
     [SerializeField] private InteriorLightBehavior[] lights;
     [SerializeField] private Color onColor, offColor;
     [SerializeField] private GameObject lightAudio;
@@ -32,7 +30,7 @@ public class ButtonBehavior : MonoBehaviour, IInteractable
         LightAmbienceBehavior lb = lightAudio.GetComponent<LightAmbienceBehavior>();
         foreach (InteriorLightBehavior item in lights)
         {
-            item.ToggleLight();
+            item.ToggleActiveState();
         }
         isOn = !isOn;
         UpdateMaterialColor();
@@ -46,12 +44,10 @@ public class ButtonBehavior : MonoBehaviour, IInteractable
         if (isOn)
         {
             m_mr.material.color = onColor;
-            immisives.material.SetColor("_Emission_Color", onEmission);
         }
         else
         {
             m_mr.material.color = offColor;
-            immisives.material.SetColor("_Emission_Color", offEmission);
         }
     }
 
