49ae40bbc075d70e5d5a6a30b8c6e5cde68fc528
Wafflgeasm <nathanael.green@g.austincc.edu>
2023-08-26 02:00:55 -0500

    Eye fix plus actual audio fix

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -94,7 +94,7 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
-        if(instance != null)
+        if(instance == null)
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
