09386d8b82b90a4027b6a1d58e32b7ecf72d7863
Wafflgeasm <nathanael.green@g.austincc.edu>
2023-11-25 23:08:44 -0600

    Options menu fix

--- Assets/Script/OptionsMenu.cs
+++ Assets/Script/OptionsMenu.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] Slider[] sliders;
+    [SerializeField] Toggle toggle;
+    [SerializeField] TMP_Dropdown dropdown;
     private void OnEnable()
     {
         sliders[0].value = AudioManager.instance.masterVolume;
         sliders[1].value = AudioManager.instance.musicVolume;
         sliders[2].value = AudioManager.instance.sfxVolume;
+        toggle.isOn = SubtitleController.hasSubtitles;
+        dropdown.value = (int)SubtitleController.language;
     }
 
 
