3ee4e72f1b6cafeea0df44e998f469e34cf77be2
MrPancakeMann <samuelj30.sm@gmail.com>
2023-11-27 00:07:48 -0600

    Tranlation is basically almost done, we need to record Spanish intro clip and Caleb needs to add all the other sound effects

--- Assets/Script/uiLanguageSprite.cs
+++ Assets/Script/uiLanguageSprite.cs
@@ -28,9 +28,11 @@ public class uiLanguageSprite : MonoBehaviour
         {
             case eSubtitleLanguage.English:
                 images.sprite = sprites[0];
+                images.GetComponent<Image>().SetNativeSize();
                 break;
             case eSubtitleLanguage.Spanish:
                 images.sprite = sprites[1];
+                images.GetComponent<Image>().SetNativeSize();
                 break;
             default:
                 Debug.Log("oof");
