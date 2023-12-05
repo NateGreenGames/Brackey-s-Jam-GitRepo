0cfcdb0b6b69ac6e8f093a55b92278cc7ad12c5d
MrPancakeMann <samuelj30.sm@gmail.com>
2023-12-05 14:32:42 -0600

    Spanish subtitles for intro clip are in

--- Assets/Script/IntroClipSubManager.cs
+++ Assets/Script/IntroClipSubManager.cs
@@ -15,7 +15,6 @@ public class IntroClipSubManager : MonoBehaviour
         switch (SubtitleController.language)
         {
             case eSubtitleLanguage.English:
-                Debug.Log("Whoopsies");
                 SubtitleController.CreateNewSubtitle("Is this thing working ?", 1.5f);
                 yield return new WaitForSeconds(1.5f);
                 SubtitleController.CreateNewSubtitle("*Ahem* We don't have much time. The submarine is the last one we have, and it's in rough shape.", 3.7f);
@@ -46,6 +45,59 @@ public class IntroClipSubManager : MonoBehaviour
                 yield return new WaitForSeconds(6f);
                 break;
             case eSubtitleLanguage.Spanish:
+                SubtitleController.CreateNewSubtitle("¿Esto funciona?", 1.6f);
+                yield return new WaitForSeconds(1.6f);
+                SubtitleController.CreateNewSubtitle("*Ahem* No tenemos mucho tiempo.", 1.9f);
+                yield return new WaitForSeconds(1.9f);
+                SubtitleController.CreateNewSubtitle("El submarino es el último que nos queda y está en mal estado", 2.6f);
+                yield return new WaitForSeconds(2.6f);
+                SubtitleController.CreateNewSubtitle("Está oxidado hasta el carajo", 1f);
+                yield return new WaitForSeconds(1f);
+                SubtitleController.CreateNewSubtitle("y los controles de navegación no están completamente operativos.", 2.55f);
+                yield return new WaitForSeconds(2.55f);
+                SubtitleController.CreateNewSubtitle("Aun así, eres nuestra última esperanza para que la humanidad sobreviva a esta catástrofe.", 4.6f);
+                yield return new WaitForSeconds(4.6f);
+                SubtitleController.CreateNewSubtitle("Todo lo que tienes que hacer es llegar a la instalación submarina,", 2.8f);
+                yield return new WaitForSeconds(2.8f);
+                SubtitleController.CreateNewSubtitle("así que enfoca tus esfuerzos en llegar allí", 2.1f);
+                yield return new WaitForSeconds(2.1f);
+                SubtitleController.CreateNewSubtitle("y las cosas deberían salir sin problemas.", 1.8f);
+                yield return new WaitForSeconds(1.8f);
+                SubtitleController.CreateNewSubtitle("Dicho esto, el submarino está equipado con una luz exterior", 3.4f);
+                yield return new WaitForSeconds(3.4f);
+                SubtitleController.CreateNewSubtitle("y lo que mejor se puede describir como una pistola eléctrica.", 3.3f);
+                yield return new WaitForSeconds(3.3f);
+                SubtitleController.CreateNewSubtitle("En el caso improbable de que te encuentres con", 2.1f);
+                yield return new WaitForSeconds(2.1f);
+                SubtitleController.CreateNewSubtitle("vida marina hostil, eso debería servir.", 3.1f);
+                yield return new WaitForSeconds(3.1f);
+                SubtitleController.CreateNewSubtitle("No olvides; la batería del submarino no puede manejar mucha carga,", 3.8f);
+                yield return new WaitForSeconds(3.8f);
+                SubtitleController.CreateNewSubtitle("así que no enciendas todo de golpe o causaras un corto circuito.", 3.3f);
+                yield return new WaitForSeconds(3.3f);
+                SubtitleController.CreateNewSubtitle("Tienes una batería, y una cantidad limitada de energía,", 2.6f);
+                yield return new WaitForSeconds(2.6f);
+                SubtitleController.CreateNewSubtitle("así que no la gastes toda: no energía significa no oxígeno, no oxígeno significa fin del juego.", 5.8f);
+                yield return new WaitForSeconds(5.8f);
+                SubtitleController.CreateNewSubtitle("Hablando de oxígeno, cuida tu tanque.", 2.3f);
+                yield return new WaitForSeconds(2.3f);
+                SubtitleController.CreateNewSubtitle("El submarino es demasiado antiguo para un electrolizador automático,", 3.4f);
+                yield return new WaitForSeconds(3.4f);
+                SubtitleController.CreateNewSubtitle("así que tendrás que rellenarlo manualmente si empiezas a quedarte sin oxígeno.", 4f);
+                yield return new WaitForSeconds(4f);
+                SubtitleController.CreateNewSubtitle("Puedes usar la válvula conectada al tanque para dirigir el oxígeno al exterior del submarino,", 4.3f);
+                yield return new WaitForSeconds(4.3f);
+                SubtitleController.CreateNewSubtitle("si por alguna razón lo necesitas.", 2.2f);
+                yield return new WaitForSeconds(2.2f);
+                SubtitleController.CreateNewSubtitle("La última cosa que necesitarás es...", 2f);
+                yield return new WaitForSeconds(2f);
+                SubtitleController.CreateNewSubtitle("oh. Mierda. ¡Mierda!", 1.5f);
+                yield return new WaitForSeconds(1.5f);
+                SubtitleController.CreateNewSubtitle("Se me acaba el tiempo; estás por tu cuenta ahora, buena suerte y...", 2.6f);
+                yield return new WaitForSeconds(2.6f);
+                SubtitleController.CreateNewSubtitle("*EXPLOSIÓN FUERTE, luego estática*", 1.5f);
+                yield return new WaitForSeconds(1.5f);
+
                 break;
             default: Debug.Log("Wtf");
                 break;
